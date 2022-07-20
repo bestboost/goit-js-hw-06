3b74b07919ee19f072aad1b89789cc71c4137293
Darya Hudzovska <102170750+bestboost@users.noreply.github.com>
2022-07-20 20:18:23 +0300

    1

--- js/task-01.js
+++ js/task-01.js
@@ -1,10 +1,12 @@
 const mainEl = document.querySelector('ul');
 console.log('Number of categories:', mainEl.children.length);
 
-// const itemList = mainEl.children;
+const itemList = mainEl.children;
+console.log(itemList);  
 
-// const itemElAll = mainEl.querySelector('.item');
-// console.log(itemElAll.firstElementChild.textContent);
+
+const itemElAll = itemList.map(item => item());
+console.log(itemElAll);
 
 const categoryEl = mainEl.querySelector('.item');
 console.log('Category:', categoryEl.firstElementChild.textContent);
