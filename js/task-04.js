61486895a37c394b13f63e6f2eb8759057654510
Darya Hudzovska <102170750+bestboost@users.noreply.github.com>
2022-07-30 22:25:58 +0300

    end task-4

--- js/task-04.js
+++ js/task-04.js
@@ -1,31 +1,16 @@
-// // Задание 4
-// // Счетчик состоит из спана и кнопок, которые, при клике, должны увеличивать
-//  и уменьшать его значение на единицу.
-
-// // <div id="counter">
-// //   <button type="button" data-action="decrement">-1</button>
-// //   <span id="value">0</span>
-// //   <button type="button" data-action="increment">+1</button>
-// // </div>
-// // Создай переменную counterValue в которой будет храниться текущее значение
-//  счетчика и инициализируй её значением 0.
-// // Добавь слушатели кликов на кнопки, внутри которых увеличивай или уменьшай
-// значение счтетчика.
-// // Обновляй интерфейс новым значением переменной counterValue.
-
 const counterButtons = document.getElementById('counter')
+
+const valueButton = document.getElementById('value');
+ let counterValue = 0;
 
 const decrementButton = counterButtons.firstElementChild;
 const incrementButton = counterButtons.lastElementChild
-console.log(decrementButton);
-console.log(incrementButton);
-
-// const decrementButton = counterButton[0].dataset.action;
-// const incrementButton = counterButton[1].dataset.action;
 
 decrementButton.addEventListener('click', () => {
-    console.log('- Klik');
+    counterValue -= 1;
+    valueButton.innerHTML = counterValue;
 }); 
 incrementButton.addEventListener('click', () => {
-    console.log('+ Klik');
+    counterValue += 1;
+    valueButton.innerHTML = counterValue;
 }); 
