cdd8e7da08c592c246beae332ca6c39ba3e0a720
Darya Hudzovska <102170750+bestboost@users.noreply.github.com>
2022-08-06 15:47:52 +0300

    make task-5

--- js/task-05.js
+++ js/task-05.js
@@ -1,31 +1,9 @@
-// Задание 5
-// Напиши скрипт который, при наборе текста в инпуте
-// input#name - input(событие input), подставляет
-// его текущее значение в span#name - output.
-// Если инпут пустой, в спане должна отображаться
-// строка "Anonymous".
-
-// < input type = "text" id = "name-input"
-// placeholder = "Please enter your name" />
-//     <h1>Hello, <span id="name-output">Anonymous
-//     </span>!</h1>
-
 const inputNamePlace = document.querySelector('#name-input');
-console.log(inputNamePlace);
 
-const outputNamePlace = document.querySelector('#name-output');
-console.log(outputNamePlace);
+const outputName = document.querySelector('#name-output');
 
-inputNamePlace.addEventListener('input', onInputEvent);
-outputNamePlace.addEventListener('output', onOutputEvent);
+inputNamePlace.addEventListener("input", (event) =>
+{
+    outputName.textContent = event.currentTarget.value;
+});
 
-function onInputEvent(event) {
-    const inputNameLetters = event.data
-    console.log(inputNameLetters);
-}
-
-function onOutputEvent(events) {
-    console.log(events);
-}
-
-// конспект input
