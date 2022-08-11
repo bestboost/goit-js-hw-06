3c2d8dc51df3db9a4bfdf34d58d72cddd3a6766d
Darya Hudzovska <102170750+bestboost@users.noreply.github.com>
2022-08-11 15:43:23 +0300

    finish task-9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+const spanBackgroundColor = document.querySelector(".color");
+
+const changeColorButton = document.querySelector(".change-color");
+
+changeColorButton.addEventListener('click', onBodyColor);
+
+function onBodyColor () {
+  document.body.style.backgroundColor = getRandomHexColor();
+  spanBackgroundColor.innerHTML = ("beforeend",'-', getRandomHexColor());
 }
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)}`; 
+}
+
+
